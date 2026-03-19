12416b1c0253b5b7a9741abb92bcbf6515d68631
Nduboi <63715705+noaroussiere@users.noreply.github.com>
2026-03-19 11:41:07 +0100

    feat(proxy): add proxy middleware for handling admin requests

---
+++ src/proxy.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+    // const token = request.cookies.get("auth_token")?.value;
+
+    // if (!token) {
+    //     return NextResponse.redirect(new URL("/login", request.url));
+    // }
+
+    // try {
+    //     await fetch(`${process.env.BACKEND_GO}/account`, {
+    //         method: "GET",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //     }).then((res) => {
+    //         if (!res.ok) {
+    //             throw new Error("Invalid token");
+    //         }
+    //     });
+    // } catch {
+    //     return NextResponse.redirect(new URL("/login", request.url));
+    // }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/admin/:path*"
+    ],
+};
