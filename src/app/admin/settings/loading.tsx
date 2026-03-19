9ac74060a9526315e6ea9e2b56dd71412a6b123a
Nduboi <63715705+noaroussiere@users.noreply.github.com>
2026-03-19 14:40:27 +0100

    refactor(loading): remove AdminRouteLoading component and its usages

--- src/app/admin/settings/loading.tsx
+++
@@ -1,5 +0,0 @@
-import AdminRouteLoading from "../_components/AdminRouteLoading";
-
-export default function Loading() {
-  return <AdminRouteLoading title="les paramètres du compte" />;
-}
--- src/app/admin/shop/new/loading.tsx
+++
@@ -1,5 +0,0 @@
-import AdminRouteLoading from "../../_components/AdminRouteLoading";
-
-export default function Loading() {
-  return <AdminRouteLoading title="la création de boutique" />;
-}
